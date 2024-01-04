0f755c38922e154fb8ed4a011313f6f121b5d637
bhanu <bhanusaireddy5244@gmail.com>
2024-01-05 01:52:47 +0530

    terraform.

--- locals.tf
+++ locals.tf
@@ -1,4 +1,12 @@
+#locals {
+  #vpc_id      = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  #app_subnets = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null),"subnets",null), "app", null),"subnet_ids", null): v.id]
+  #}
+
 locals {
-  vpc_id      = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  app_subnets = [for k, v in lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null) : v.id]
-  }
+  vpc_id = coalesce(lookup(lookup(module.vpc, "main", {}), "vpc_id", null), "")
+
+  app_subnets = [
+    for k, v in coalesce(lookup(lookup(lookup(lookup(module.vpc, "main", {}), "subnets", {}), "app", {}), "subnet_ids", null), []): v.id
+  ]
+}
