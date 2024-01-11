0aad020fd8cfa0989eaf5e0a7623b13bd10e90ed
bhanu <bhanusaireddy5244@gmail.com>
2024-01-11 10:53:19 +0530

    terraform.

--- main.tf
+++ main.tf
@@ -35,4 +35,10 @@ module "docdb" {
   env = var.env
   for_each = var.docdb
   subnets_ids = local.db_subnets
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  vpc_id = vpc_id
+  sg_ingress_cidr = local.app_subnets_cidr
+  vpc_security_group_ids = [aws_security_group.main.id]
 }
