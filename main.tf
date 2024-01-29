b2710567502ac238e1080cfe431f6e9cf091c0cc
bhanu <bhanusaireddy5244@gmail.com>
2024-01-29 18:53:14 +0530

    terraform.

--- main.tf
+++ main.tf
@@ -128,6 +128,6 @@ module "app" {
   sg_ingress_cidr  = local.app_subnets_cidr
   vpc_id           = local.vpc_id
   subnet_ids       = local.app_subnets
-  alb_name = lookup(lookup(lookup(module.alb, "private", null), "dns_name", null), 0, null)
+  alb_name = lookup(lookup(lookup(module.alb, "private", null), "alb", null), "dns_name", null)
 }
 
