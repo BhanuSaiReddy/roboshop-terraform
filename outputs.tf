571b25bd50b3ddad52a5917eb03d83d445576e87
bhanu <bhanusaireddy5244@gmail.com>
2024-01-29 18:24:00 +0530

    terraform.

--- outputs.tf
+++ outputs.tf
@@ -2,21 +2,7 @@ output "vpc" {
   value = data.aws_subnets.subnets.ids
 }
 
-resource "null_resource" "APPS_resource" {
-  provisioner "local-exec" {
-    command = "echo -e '\\x1b[34mAPPS SETUP COMPLETED SUCCESSFULLY\\x1b[0m'"
-  }
-}
 
-#output "vpc" {
- # value = module.vpc
-#}
-
-resource "null_resource" "dev_resource" {
-  provisioner "local-exec" {
-    command = "echo -e '\\x1b[34mDEV SETUP COMPLETED SUCCESSFULLY\\x1b[0m'"
-  }
-}
 
 output "alb" {
   value = module.alb
